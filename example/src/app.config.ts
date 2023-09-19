8c1c03d71f4515e8235e71d39e364e79287d6020
Benjamin Janssens <benji.janssens@gmail.com>
2023-09-19 17:35:30 +0200

    add database url to example app config

--- example/src/app.config.ts
+++ example/src/app.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 import { Environment, validateConfig } from '@icapps/nestkit-common';
 import { Expose } from 'class-transformer';
-import { IsEnum, IsPort } from 'class-validator';
+import { IsEnum, IsPort, IsUrl } from 'class-validator';
 
 class AppConfig {
   @Expose({ name: 'NODE_ENV' })
@@ -11,6 +11,10 @@ class AppConfig {
   @Expose({ name: 'PORT' })
   @IsPort()
   port: string;
+
+  @Expose({ name: 'DATABASE_URL' })
+  @IsUrl()
+  databaseUrl: string;
 }
 
 export default registerAs('app', () => validateConfig(AppConfig));
